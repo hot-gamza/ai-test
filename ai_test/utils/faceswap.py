69c97ae66c3fb74cf47a49f602effd5973e32b59
lakecrimsonn <lakecrimsonn@gmail.com>
2023-09-12 14:09:40 +0900

    flask gogo

--- ai_test/utils/faceswap.py
+++ ai_test/utils/faceswap.py
@@ -4,39 +4,36 @@ import datetime
 import numpy as np
 from PIL import Image
 from insightface.app import FaceAnalysis
+from gfpgan_model import gfpgan_gogo
 
 app = FaceAnalysis(name='buffalo_l')
 app.prepare(ctx_id=0, det_size=(640, 640))
 swapper = insightface.model_zoo.get_model(
-    r'models\inswapper_128.onnx', download=False, download_zip=False)
+    r'../models/inswapper_128.onnx', download=False, download_zip=False)
 
 
-def faceswap(face_img):
+def faceswap(template_img, face_img):
     '''
-        faceswap(합성 얼굴 이미지)
+        faceswap(템플릿 이미지, 합성 얼굴 이미지)
     '''
 
-    img_arr = [
-        r'images\st1.jpg'
-    ]
+    template_img = cv2.imread(template_img)
+    template_faces = app.get(template_img)
+    print("detected number of faces: ", len(template_faces))
+    dn = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    fn = f'fs_{dn}.jpg'
+    face_img_read = cv2.imread(face_img)
+    copy_face = app.get(face_img_read)[0]
 
-    for source_img in img_arr:
-        source_img = cv2.imread(source_img)
-        faces = app.get(source_img)
-        print("detected number of faces: ", len(faces))
-        dn = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        fn = f'fs_{dn}.jpg'
-        face_img_read = cv2.imread(face_img)
-        copy_face = app.get(face_img_read)[0]
+    for find_face in template_faces:
+        result = swapper.get(template_img, find_face, copy_face)
 
-        for find_face in faces:
-            result = swapper.get(source_img, find_face, copy_face)
+    gfp_result = np.array(gfpgan_gogo(result))
+    cv2.imwrite(f'../images/result/{fn}', gfp_result)
+    print(f"saved a file successfully. {fn}")
 
-        cv2.imwrite(f'images/{fn}', result)
-        print(f"saved a file successfully. {fn}")
-
-    return None
+    return gfp_result
 
 
 if __name__ == '__main__':
-    faceswap(r'images\3-1000x1500.jpg')
+    faceswap(r'../images/st1.jpg',r'../images/karina.jpg')
--- ai_test/utils/gfpgan_model.py
+++ ai_test/utils/gfpgan_model.py
@@ -4,12 +4,16 @@ import numpy as np
 from PIL import Image
 
 def gfpgan_gogo(img):
-    img = Image.open(img)
+    '''
+        gfp 업스케일링 적용
+        gfpgan_gogo(페이스 스왑한 이미지)
+    '''
+    img = Image.fromarray(img)
     original_img = img.copy()
     np_img = np.array(img)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model = GFPGANer(model_path=r'models/GFPGANv1.4.pth', upscale=1, arch='clean', channel_multiplier=2, bg_upsampler=None, device=device)
+    model = GFPGANer(model_path=r'../models/GFPGANv1.4.pth', upscale=1, arch='clean', channel_multiplier=2, bg_upsampler=None, device=device)
     np_img_bgr = np_img[:, :, ::-1]
     _, _, gfpgan_output_bgr = model.enhance(np_img_bgr, has_aligned=False, only_center_face=False, paste_back=True)
     np_img = gfpgan_output_bgr[:, :, ::-1]
@@ -19,7 +23,7 @@ def gfpgan_gogo(img):
         original_img, restored_img, 1
     )
 
-    result_img.show()
+    return result_img    
 
 if __name__ == '__main__':
     gfpgan_gogo('ej2.jpg')
