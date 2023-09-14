a9d9475a10f761daea229a7f43b4a71e6b8df0ce
lakecrimsonn <lakecrimsonn@gmail.com>
2023-09-14 23:02:20 +0900

    23:02

--- ai_test/views/main_routes.py
+++ ai_test/views/main_routes.py
@@ -3,7 +3,6 @@ from ..utils.upload import handle_upload
 from ..utils.faceswap import faceswap
 import os
 import stat
-import base64
 from io import BytesIO
 from PIL import Image
 
@@ -15,10 +14,9 @@ if not os.path.exists(final_results_dir):
     # 권한설정
     os.chmod(final_results_dir, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
-        
         male_files = request.files.getlist('imgFile')[0]
         female_files = request.files.getlist('imgFile')[1]
 
@@ -27,24 +25,8 @@ def index():
         male_filenames = handle_upload(male_files)
         female_filenames = handle_upload(female_files)
 
-        # print(type(male_filenames), male_filenames)
-
         template_img = r"D:\project\ai-test\ai_test\images\template\temp.jpg"
 
         # 이미 faceswap 함수가 모든 처리를 해서 결과 파일을 저장하고, 그 경로를 반환
         result_filenames = faceswap(template_img, male_filenames, female_filenames)
-
-        # final_result_filenames = []
-
-        # for result_file_path in result_filenames:
-        #     if os.path.exists(result_file_path):
-        #         final_result_filename = os.path.join("final_results", os.path.basename(result_file_path))
-        #         os.rename(result_file_path, os.path.join("static", final_result_filename))
-        #         final_result_filenames.append(final_result_filename)
-        #         print("final_result_filename:", final_result_filename)
-        #     else:
-        #         print(f"File does not exist: {result_file_path}")
-
-        return render_template('result.html', filenames=result_filenames)
-
-    return render_template('index.html')
+        return result_filenames
